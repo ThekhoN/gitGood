597418b10739ac7bf7bf723d30c0f382efcfe0b0
Ngaosathe <662318@MACPC271482.local>
2018-08-03 13:58:16 +0530

    new branch debug for testing

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,6 @@
 const sayHelloWorld = () => {
-    console.log('saying hello world!!!')
+    // console.log('saying hello world!!!')
+    console.log("The world said: Hello", "The worst first and the last best");
 }
 
 export default sayHelloWorld
